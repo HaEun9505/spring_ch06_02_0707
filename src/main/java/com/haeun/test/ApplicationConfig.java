f7600e1270e54ee1ccff3b5ec5fae8d64526a392
ATE <ATE@DESKTOP-ODIPDNU>
2022-07-13 10:20:11 +0900

    스프링 빈 자바 설정(주석)

--- src/main/java/com/haeun/test/ApplicationConfig.java
+++ src/main/java/com/haeun/test/ApplicationConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 //DI 설정해주는 Container클래스 - 스프링 설정에 사용되는 클래스
 @Configuration
 public class ApplicationConfig {
-	
+	//Bean으로 사용되는 메소드(bean 객체 생성)
 	@Bean
 	public Student student1() {	//반환되는 자료타입이 Student클래스, student1:bean의 이름(id)
 		
@@ -15,27 +15,30 @@ public class ApplicationConfig {
 		hobbys.add("운동");
 		hobbys.add("노래");
 		
-		//student: bean 이름
+		//생성자에 설정(constructor)
 		Student student = new Student("홍길동", 25, hobbys);
 		
-		//생성자 없으므로, setter
+		//생성자 없으므로, setter(property에 설정)
 		student.setWeight(80);
 		student.setHeight(180);
+		
 		return student;
 	}
 	@Bean
 	public Student student2() {	//반환되는 자료타입이 Student클래스, student2:bean의 이름(id)
 		
+		//생성자에 설정(constructor)
 		ArrayList<String> hobbys = new ArrayList<String>();	//배열 객체
 		hobbys.add("달리기");
 		hobbys.add("자전거");
 		
-		//student: bean 이름
+		//생성자에 설정(constructor)
 		Student student = new Student("김유신", 25, hobbys);
 		
-		//생성자 만들지 않았으므로, setter로 설정
+		//생성자 없으므로, setter(property에 설정)
 		student.setWeight(60);
 		student.setHeight(150);
+		
 		return student;
 	}
 	
